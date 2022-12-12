850d5b0c4cd4dfa7438e1b03142406da7145a0f5
tony <s290970178@gmail.com>
2022-12-12 14:25:54 +0000

    first commit

--- WIoTApi/app_config.py
+++ WIoTApi/app_config.py
@@ -4,12 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import connexion
 from flask_cors import CORS
+import logging
 
-application = connexion.FlaskApp(__name__)
-
+appl = connexion.FlaskApp(__name__)
 # Read the swagger.yml file to configure the endpoints
-application.add_api("swagger.yaml")
-app = application.app
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@192.168.231.144:3306/openapi"
+appl.add_api("swagger.yaml")
+app = appl.app
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@192.168.44.137:3306/openapi"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- WIoTApi/main.py
+++ WIoTApi/main.py
@@ -1,6 +1,8 @@
 from app_config import app
 from model import db
+from flask_cors import CORS
 
 if __name__ == '__main__':
+    CORS(app)
     db.init_app(app)
     app.run(host='0.0.0.0', debug=True, port=5000)
